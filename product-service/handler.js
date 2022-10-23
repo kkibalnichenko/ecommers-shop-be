7f79f2891ef2fc791660ca9c4b25bc3444dd4c08
Kristina Kibalnichenko <kristina_kibalnichenko@epam.com>
2022-10-23 17:04:32 +0200

    dotenv config deleted

--- product-service/handler.js
+++ product-service/handler.js
@@ -2,6 +2,5 @@
 const getProductsList = require('./handlers/getProductsList');
 const getProductsById = require('./handlers/getProductsById');
 const createProduct = require('./handlers/createProduct');
-require('dotenv').config();
 
 module.exports = { getProductsList, getProductsById, createProduct };
